99c8750f2c357d3e87b8a026e1fe9c6299973f1e
bschuedzig <benjamin.schuedzig@mantro.net>
2017-11-13 11:35:15 +0100

    [BUGFIX] Reenabled tests (and mandatory eslint)

--- test/strings-test.js
+++ test/strings-test.js
@@ -2,25 +2,24 @@ const assert = require('assert');
 
 var stringsOfSet = require('../lib/strings').stringsOfSet;
 
-const BASE64_CHARS = require('../lib/consts').BASE64_CHARS;
-const HEX_CHARS = require('../lib/consts').HEX_CHARS;
+const options = require('../lib/options');
 
 describe('strings', () => {
   describe('stringsOfSet', () => {
     it('should not return if not enough HEX_CHARS found', () => {
-      let strings = stringsOfSet('test', HEX_CHARS);
+      let strings = stringsOfSet('test', options.HEX_CHARS);
       assert.equal(strings.length, 0, 'Strings cannot be found if threshold not matched');
     });
 
     it('should return strings if enough HEX_CHARS found', () => {
-      let strings = stringsOfSet('122312231223122312231xx444444443333AAAAAAAAA', HEX_CHARS);
+      let strings = stringsOfSet('122312231223122312231xx444444443333AAAAAAAAA', options.HEX_CHARS);
       assert.equal(strings.length, 2, 'All matched strings should be returned');
       assert.equal(strings[0], '122312231223122312231');
       assert.equal(strings[1], '444444443333AAAAAAAAA');
     });
 
     it('should return strings if enough BASE64_CHARS found', () => {
-      let strings = stringsOfSet('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789', BASE64_CHARS);
+      let strings = stringsOfSet('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789', options.BASE64_CHARS);
       assert.equal(strings.length, 1, 'All matched strings should be returned');
       assert.equal(strings[0], 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789');
     });
